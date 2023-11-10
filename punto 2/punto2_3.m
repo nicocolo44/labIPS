0137c810eda58bf83f1a962735eef6824e5a0ae8
Nicolas Colombini <nicolascolombini44@gmail.com>
2023-11-10 12:56:44 -0300

    cambiando el sistema, aparecen ecos

--- punto 2/punto2_3.m
+++ punto 2/punto2_3.m
@@ -4,12 +4,11 @@ addpath(fullfile(scriptDir, '..', 'funciones'));
 addpath(fullfile(scriptDir, '..', 'punto 2\sistemas'));
 
 [x, fs] = audioread('audio.wav');
-
 subplot(2,1,1);
 stem(x);
-sound(x,fs);
+%sound(x,fs);
 
-y = sistema2(length(x),x);
+y = sistema2(x);
 
 sound(y, fs);
 subplot(2,1,2);
